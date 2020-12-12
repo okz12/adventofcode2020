4431ea62e9a10c5a3e75c547af6dad7823b248ff
Osman <osmanzubair658@gmail.com>
2020-12-12 16:57:05 +0000

    d12 cleaner

--- 12/12.py
+++ 12/12.py
@@ -1,87 +1,35 @@
 from __future__ import annotations
-from dataclasses import dataclass
+from typing import List
 
-dirs = ["N", "E", "S", "W"]
+dir_map = {"N": 1j, "E": 1, "S": -1j, "W": -1, "L": 1j, "R": -1j}
 
+class WaypointAndShip:
 
-@dataclass
-class FloatingObject:
-    x: int = 0
-    y: int = 0
+    def __init__(self, ship: complex = 0 + 0j, waypoint: complex = 1 + 0j, move_waypoint: bool = False):
+        self.ship = ship
+        self.waypoint = waypoint
+        self.move_waypoint = move_waypoint
 
-    def run(self, inst: str) -> None:
-        op, val = inst[0], int(inst[1:])
-        if op in {"L", "R", "F"}:
-            self.rotate(op, val)
-        else:
-            self.move(op, val)
+    def run(self, instructions: List[str]) -> int:
+        for inst in instructions:
+            op, val = inst[0], int(inst[1:])
+            if op in {"L", "R"}:# condition 2
+                self.waypoint *= (dir_map[op] ** (val // 90))
+            elif op == "F":
+                self.ship += self.waypoint * val
+            elif op in {'N', 'E', 'S', 'W'}:
+                if self.move_waypoint:
+                    self.waypoint += dir_map[op] * val
+                else:
+                    self.ship += dir_map[op] * val
+            else:
+                raise ValueError (f"{op} not a valid command")
+            # print(f"{inst}:\t{self}")
+        return int(abs(self.ship.real) + abs(self.ship.imag))
 
-    def rotate(self, op, val):
-        raise NotImplementedError
+    def __str__(self) -> str:
+        return f"Ship: {int(self.ship.real)}, {int(self.ship.imag)} \tWaypoint: {int(self.waypoint.real)}, {int(self.waypoint.imag)}"
 
-    def move(self, op, val):
-        if op == "N":
-            self.y += val
-        elif op == "S":
-            self.y -= val
-        elif op == "E":
-            self.x += val
-        elif op == "W":
-            self.x -= val
-        else:
-            raise ValueError({f"{op} not valid"})
-
-    def m_dist(self):
-        return abs(self.x) + abs(self.y)
-
-@dataclass
-class Ship(FloatingObject):
-    d: str = "E"
-
-    def rotate(self, op, val):
-        if op == "L":
-            self.d = dirs[dirs.index(self.d) - (val//90)]
-        elif op == "R":
-            self.d = dirs[(dirs.index(self.d) + (val//90)) % len(dirs)]
-        elif op == "F":
-            self.move(self.d, val)
-
-    def __str__(self):
-        return f"Ship: {self.x}, {self.y} D: {self.d}"
-
-@dataclass
-class Waypoint(FloatingObject):
-    ship: Ship = None
-    x: int = 10
-    y: int = 1
-
-    def __init__(self):
-        self.ship = Ship()
-
-    def rotate(self, op, val):
-        if op == "F":
-            self.ship.x += self.x * val
-            self.ship.y += self.y * val
-        elif val == 180:
-            self.x, self.y = -self.x, -self.y
-        elif (op, val) == ("L", 90) or (op, val) == ("R", 270):
-            #
-            self.x, self.y = -self.y, self.x
-        elif (op, val) == ("R", 90) or (op, val) == ("L", 270):
-            self.x, self.y = self.y, -self.x
-
-    def m_dist(self):
-        return self.ship.m_dist()
-
-    def __str__(self):
-        return f"Waypoint: {self.x}, {self.y} - {self.ship} "
-
-def find_dist(string:str, fp: FloatingObject) -> int:
-    instructions = string.split("\n")
-    for i in instructions:
-        fp.run(i)
-        # print(f"{i}:\t{str(fp)}")
-    return fp.m_dist()
 
 if __name__ == "__main__":
     testcase = """\
@@ -94,8 +42,8 @@ F11"""
     with open('input.txt', 'r') as f:
         data = f.read()
 
-    assert find_dist(testcase, Ship()) == 25
-    print(find_dist(data, Ship()))
+    assert WaypointAndShip().run(testcase.split("\n")) == 25
+    print(WaypointAndShip().run(data.split("\n")))
 
-    assert find_dist(testcase, Waypoint()) == 286
-    print(find_dist(data, Waypoint()))
+    assert WaypointAndShip(waypoint = 10 + 1j, move_waypoint = True).run(testcase.split("\n")) == 286
+    print(WaypointAndShip(waypoint = 10 + 1j, move_waypoint = True).run(data.split("\n")))
