c67243b5c7497c29ab5203763fa6c593d1a8b23e
Osman <osmanzubair658@gmail.com>
2020-12-15 08:48:14 +0000

    d15 cleaner

--- 15/15.py
+++ 15/15.py
@@ -3,20 +3,20 @@ from typing import List
 def spoken(A: List[int], num: int = 2020):
     memory, curr = {v: i for i, v in enumerate(A)}, 0
     for i in range(len(A), num-1):
-        memory[curr], curr = i, i - memory.get(curr, i) #0 if not in spoken
+        memory[curr], curr = i, i - memory.get(curr, i) # 0 if not in memory
     return curr
 
 if __name__ == "__main__":
     data = [16, 11, 15, 0, 1, 7]
 
-    assert spoken([0, 3, 6]) == 436
-    assert spoken([1, 3, 2]) == 1
-    assert spoken([2, 1, 3]) == 10
-    assert spoken([1, 2, 3]) == 27
-    assert spoken([2, 3, 1]) == 78
-    assert spoken([3, 2, 1]) == 438
-    assert spoken([3, 1, 2]) == 1836
-    print(spoken(data))
+    assert spoken([0, 3, 6], 2020) == 436
+    assert spoken([1, 3, 2], 2020) == 1
+    assert spoken([2, 1, 3], 2020) == 10
+    assert spoken([1, 2, 3], 2020) == 27
+    assert spoken([2, 3, 1], 2020) == 78
+    assert spoken([3, 2, 1], 2020) == 438
+    assert spoken([3, 1, 2], 2020) == 1836
+    print(spoken(data, 2020))
 
     assert spoken([0, 3, 6], 30000000) == 175594
     assert spoken([1, 3, 2], 30000000) == 2578
