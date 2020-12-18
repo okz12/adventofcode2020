1eeba887f0a204eccc105f41335985facb8e373f
Osman <osmanzubair658@gmail.com>
2020-12-18 04:40:27 +0000

    formatting

--- 05/05_functional.py
+++ 05/05_functional.py
@@ -1,6 +1,11 @@
-seat_id = lambda x: int("".join(map({'B': '1', 'F': '0', 'L': '0', 'R': '1'}.get, x)), 2)
+from typing import Tuple
 
-def get_seats(passes: str) -> int:
+seat_id = lambda x: int(
+    "".join(map({"B": "1", "F": "0", "L": "0", "R": "1"}.get, x)), 2
+)
+
+
+def get_seats(passes: str) -> Tuple[int, int]:
     passes_ = [seat_id(pass_) for pass_ in passes.split("\n")]
     sum_, min_, max_ = sum(passes_), min(passes_), max(passes_)
     return max_, int(((max_ - min_ + 1) * (max_ + min_) / 2) - sum_)
@@ -11,7 +16,7 @@ if __name__ == "__main__":
     assert seat_id("FFFBBBFRRR") == 119
     assert seat_id("BBFFBBFRLL") == 820
 
-    with open('input.txt', 'r') as f:
+    with open("input.txt", "r") as f:
         data = f.read()
 
     print(get_seats(data))
