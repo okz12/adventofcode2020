3675955479717063d6535cef361dfdae5919354e
Osman <osmanzubair658@gmail.com>
2020-12-25 05:17:23 +0000

    day 25

---
+++ 25/25.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+import re
+from typing import Dict, List, Set, Tuple
+from dataclasses import dataclass
+
+
+def find_loop_size(encryption_key: int) -> int:
+    value = 1
+    subject_number = 7
+    loop_size = 0
+    while value != encryption_key:
+        value = (value * subject_number) % 20201227
+        loop_size += 1
+    return loop_size
+
+
+def create(loop_size: int, subject_number: int) -> int:
+    value = 1
+    for _ in range(loop_size):
+        value = (value * subject_number) % 20201227
+    return value
+
+
+if __name__ == "__main__":
+    testcase = (17807724, 5764801)
+    data = (16915772, 18447943)
+
+    assert create(find_loop_size(testcase[0]), testcase[1]) == 14897079
+    print(create(find_loop_size(data[0]), data[1]))
